dc2ad711a73da3ab18b20718d5a833caa748c9ed
Caleb Collins <cmikec12@gmail.com>
2022-04-29 20:52:20 -0400

    Updated styling and responsiveness

--- script.js
+++ script.js
@@ -1,17 +1,561 @@
-let str = "open come into people america touch slight tin crip walk";
+let wordbank = [
+  "the",
+  "of",
+  "to",
+  "and",
+  "a",
+  "in",
+  "is",
+  "it",
+  "you",
+  "that",
+  "he",
+  "was",
+  "for",
+  "on",
+  "are",
+  "with",
+  "as",
+  "his",
+  "they",
+  "be",
+  "at",
+  "one",
+  "have",
+  "this",
+  "from",
+  "or",
+  "had",
+  "by",
+  "hot",
+  "but",
+  "some",
+  "what",
+  "there",
+  "we",
+  "can",
+  "out",
+  "other",
+  "were",
+  "all",
+  "your",
+  "when",
+  "up",
+  "use",
+  "word",
+  "how",
+  "said",
+  "an",
+  "each",
+  "she",
+  "which",
+  "do",
+  "their",
+  "time",
+  "if",
+  "will",
+  "way",
+  "about",
+  "many",
+  "then",
+  "them",
+  "would",
+  "write",
+  "like",
+  "so",
+  "these",
+  "her",
+  "long",
+  "make",
+  "thing",
+  "see",
+  "him",
+  "two",
+  "has",
+  "look",
+  "more",
+  "day",
+  "could",
+  "go",
+  "come",
+  "did",
+  "my",
+  "sound",
+  "no",
+  "most",
+  "number",
+  "who",
+  "over",
+  "know",
+  "water",
+  "than",
+  "call",
+  "first",
+  "people",
+  "may",
+  "down",
+  "side",
+  "been",
+  "now",
+  "find",
+  "any",
+  "new",
+  "work",
+  "part",
+  "take",
+  "get",
+  "place",
+  "made",
+  "live",
+  "where",
+  "after",
+  "back",
+  "little",
+  "only",
+  "round",
+  "man",
+  "year",
+  "came",
+  "show",
+  "every",
+  "good",
+  "me",
+  "give",
+  "our",
+  "under",
+  "name",
+  "very",
+  "through",
+  "just",
+  "form",
+  "much",
+  "great",
+  "think",
+  "say",
+  "help",
+  "low",
+  "line",
+  "before",
+  "turn",
+  "cause",
+  "same",
+  "mean",
+  "differ",
+  "move",
+  "right",
+  "derek",
+  "boy",
+  "old",
+  "too",
+  "does",
+  "tell",
+  "sentence",
+  "set",
+  "three",
+  "want",
+  "air",
+  "well",
+  "also",
+  "play",
+  "small",
+  "end",
+  "put",
+  "home",
+  "read",
+  "hand",
+  "port",
+  "large",
+  "spell",
+  "add",
+  "even",
+  "land",
+  "here",
+  "must",
+  "big",
+  "high",
+  "such",
+  "follow",
+  "act",
+  "why",
+  "ask",
+  "men",
+  "change",
+  "went",
+  "light",
+  "kind",
+  "off",
+  "need",
+  "house",
+  "picture",
+  "try",
+  "us",
+  "again",
+  "animal",
+  "point",
+  "mother",
+  "world",
+  "near",
+  "build",
+  "self",
+  "earth",
+  "father",
+  "head",
+  "stand",
+  "own",
+  "page",
+  "should",
+  "country",
+  "found",
+  "answer",
+  "school",
+  "grow",
+  "study",
+  "still",
+  "learn",
+  "plant",
+  "cover",
+  "food",
+  "sun",
+  "four",
+  "thought",
+  "let",
+  "keep",
+  "eye",
+  "never",
+  "last",
+  "door",
+  "between",
+  "city",
+  "tree",
+  "cross",
+  "since",
+  "hard",
+  "start",
+  "might",
+  "story",
+  "saw",
+  "far",
+  "sea",
+  "draw",
+  "left",
+  "late",
+  "run",
+  "don't",
+  "while",
+  "press",
+  "close",
+  "night",
+  "real",
+  "life",
+  "few",
+  "stop",
+  "open",
+  "seem",
+  "together",
+  "next",
+  "white",
+  "children",
+  "begin",
+  "got",
+  "walk",
+  "example",
+  "ease",
+  "paper",
+  "often",
+  "always",
+  "music",
+  "those",
+  "both",
+  "mark",
+  "book",
+  "letter",
+  "until",
+  "mile",
+  "river",
+  "car",
+  "feet",
+  "care",
+  "second",
+  "group",
+  "carry",
+  "took",
+  "rain",
+  "eat",
+  "room",
+  "friend",
+  "began",
+  "idea",
+  "fish",
+  "mountain",
+  "north",
+  "once",
+  "base",
+  "hear",
+  "horse",
+  "cut",
+  "sure",
+  "watch",
+  "color",
+  "face",
+  "wood",
+  "main",
+  "enough",
+  "plain",
+  "girl",
+  "usual",
+  "young",
+  "ready",
+  "above",
+  "ever",
+  "red",
+  "list",
+  "though",
+  "feel",
+  "talk",
+  "bird",
+  "soon",
+  "body",
+  "dog",
+  "family",
+  "direct",
+  "pose",
+  "leave",
+  "song",
+  "measure",
+  "state",
+  "product",
+  "black",
+  "short",
+  "numeral",
+  "class",
+  "wind",
+  "question",
+  "happen",
+  "complete",
+  "ship",
+  "area",
+  "half",
+  "rock",
+  "order",
+  "fire",
+  "south",
+  "problem",
+  "piece",
+  "told",
+  "knew",
+  "pass",
+  "farm",
+  "top",
+  "whole",
+  "king",
+  "size",
+  "heard",
+  "best",
+  "hour",
+  "better",
+  "during",
+  "hundred",
+  "am",
+  "remember",
+  "step",
+  "early",
+  "hold",
+  "west",
+  "ground",
+  "interest",
+  "reach",
+  "fast",
+  "five",
+  "sing",
+  "listen",
+  "six",
+  "table",
+  "travel",
+  "less",
+  "morning",
+  "ten",
+  "simple",
+  "several",
+  "vowel",
+  "toward",
+  "war",
+  "lay",
+  "against",
+  "pattern",
+  "slow",
+  "center",
+  "love",
+  "person",
+  "money",
+  "serve",
+  "appear",
+  "road",
+  "map",
+  "science",
+  "rule",
+  "govern",
+  "pull",
+  "cold",
+  "notice",
+  "voice",
+  "fall",
+  "power",
+  "town",
+  "fine",
+  "certain",
+  "fly",
+  "unit",
+  "lead",
+  "cry",
+  "dark",
+  "machine",
+  "note",
+  "wait",
+  "plan",
+  "figure",
+  "star",
+  "box",
+  "noun",
+  "field",
+  "rest",
+  "correct",
+  "able",
+  "pound",
+  "done",
+  "beauty",
+  "drive",
+  "stood",
+  "contain",
+  "front",
+  "teach",
+  "week",
+  "final",
+  "gave",
+  "green",
+  "oh",
+  "quick",
+  "develop",
+  "sleep",
+  "warm",
+  "free",
+  "minute",
+  "strong",
+  "special",
+  "mind",
+  "behind",
+  "clear",
+  "tail",
+  "produce",
+  "fact",
+  "street",
+  "inch",
+  "lot",
+  "nothing",
+  "course",
+  "stay",
+  "wheel",
+  "full",
+  "force",
+  "blue",
+  "object",
+  "decide",
+  "surface",
+  "deep",
+  "moon",
+  "island",
+  "foot",
+  "yet",
+  "busy",
+  "test",
+  "record",
+  "boat",
+  "common",
+  "gold",
+  "possible",
+  "plane",
+  "age",
+  "dry",
+  "wonder",
+  "laugh",
+  "thousand",
+  "ago",
+  "ran",
+  "check",
+  "game",
+  "shape",
+  "yes",
+  "hot",
+  "miss",
+  "brought",
+  "heat",
+  "snow",
+  "bed",
+  "bring",
+  "sit",
+  "perhaps",
+  "fill",
+  "east",
+  "weight",
+  "language",
+  "among",
+];
 
-let divtoadd = document.getElementById("textdiv");
+// Get HTML elements
+let promptdiv = document.getElementById("promptdiv");
+let typeinput = document.getElementById("typeinput");
+let timeoutput = document.getElementById("timeoutput");
+let resetbtn = document.getElementById("resetbtn");
+let wpmoutput = document.getElementById("wpmoutput");
 
-let typeinme = document.getElementById("type");
+// Set defaults
+let line;
+let numwords = 10;
 
-let timer = document.getElementById("timer");
+function createword() {
+  promptdiv.replaceChildren();
 
-let butt = document.getElementById("resetbtn");
+  line = "";
 
-for (const c of str.split("")) {
-  let span = document.createElement("span");
-  span.innerHTML = c;
-  divtoadd.appendChild(span);
+  if (isNaN(numwords)) {
+    line =
+      "I can't do api requests using online editor haha. I spent 30 minutes trying to figure out what the fuck was wrong for no reason lfg.";
+  } else {
+    for (let i = 0; i < numwords; i++) {
+      line += wordbank[Math.floor(Math.random() * wordbank.length)] + " ";
+    }
+
+    line = line.slice(0, -1);
+  }
+
+  for (const c of line.split("")) {
+    let span = document.createElement("span");
+    span.innerHTML = c;
+    promptdiv.appendChild(span);
+  }
+}
+
+// Create default prompt
+createword();
+
+// Text highlighting
+let wordcountbtns = document.getElementsByClassName("wordcountbtn");
+
+for (const btn of wordcountbtns) {
+  btn.addEventListener("click", function (event) {
+    if (isNaN(btn.innerHTML)) {
+      numwords = "quote";
+    } else {
+      numwords = btn.innerHTML;
+    }
+
+    for (const b of wordcountbtns) {
+      b.style.color = "rgb(211, 211, 211)";
+    }
+
+    btn.style.color = "rgb(109, 209, 139)";
+
+    reset();
+  });
 }
 
 let started = false;
@@ -19,22 +563,22 @@ let startTime;
 let start;
 let totaltime;
 
-typeinme.addEventListener("input", function (event) {
-  let mytext = typeinme.value; //You already have the element as a variable
-  let splittext = mytext.split("");
-  let spans = divtoadd.children;
+typeinput.addEventListener("input", function (event) {
+  let typeinputtext = typeinput.value;
+  let splittext = typeinputtext.split("");
+  let spans = promptdiv.children;
 
   if (!started) {
     started = true;
     startTime = Date.now();
-    start = setInterval(startTimer, 1);
+    start = setInterval(starttimeoutput, 10);
   }
 
   let allcorrect = true;
 
   for (let x = 0; x < spans.length; x++) {
     if (splittext[x] == null) {
-      spans[x].style.color = "black";
+      spans[x].style.color = "lightslategray";
       spans[x].style.textDecoration = "none";
       allcorrect = false;
     } else if (splittext[x] !== spans[x].innerHTML) {
@@ -42,7 +586,7 @@ typeinme.addEventListener("input", function (event) {
       spans[x].style.textDecoration = "underline";
       allcorrect = false;
     } else {
-      spans[x].style.color = "green";
+      spans[x].style.color = "white";
       spans[x].style.textDecoration = "none";
     }
   }
@@ -50,24 +594,41 @@ typeinme.addEventListener("input", function (event) {
   if (allcorrect) {
     started = false;
     clearInterval(start);
-    document.getElementById("wpm").innerHTML = Math.round((str.length / 5 / totaltime) * 60) + " wpm";
+    wpmoutput.innerHTML = Math.round((line.length / 5 / totaltime) * 60) + " wpm";
+    createword();
+    typeinput.value = "";
+  }
+
+  if (typeinputtext == "tappytype") {
+    document.getElementById("title").innerHTML = "derekshatypes150wpm";
   }
 });
 
-function startTimer() {
+function starttimeoutput() {
   totaltime = Math.round(100 * ((Date.now() - startTime) / 1000)) / 100;
-  document.getElementById("wpm").innerHTML = Math.round((type.value.length / 5 / totaltime) * 60) + " wpm";
-  timer.innerHTML = totaltime + " sec";
+  wpmoutput.innerHTML = Math.round((typeinput.value.length / 5 / totaltime) * 60) + " wpm";
+  timeoutput.innerHTML = totaltime + " sec";
 }
 
-butt.addEventListener("click", function (event) {
-  typeinme.value = "";
-  started = false;
-  clearInterval(start);
-  document.getElementById("wpm").innerHTML = "0 wpm";
-  timer.innerHTML = "0.00" + " sec";
-  for (const y of divtoadd.children) {
-    y.style.color = "black";
-    y.style.textDecoration = "none";
+resetbtn.addEventListener("click", reset);
+
+document.addEventListener("keyup", function (event) {
+  if (event.key == "Escape" || event.key == "Tab") {
+    reset();
+    typeinput.focus();
   }
 });
+
+function reset() {
+  typeinput.value = "";
+  started = false;
+  clearInterval(start);
+  wpmoutput.innerHTML = "0 wpm";
+  timeoutput.innerHTML = "0.00" + " sec";
+  for (const y of promptdiv.children) {
+    y.style.color = "lightslategray";
+    y.style.textDecoration = "none";
+  }
+  createword();
+  typeinput.focus();
+}
